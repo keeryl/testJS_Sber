99caf2a9b9a265aacd29d4c99f21bb351dd8c126
Кирилл Зикевский <2309696@mail.ru>
2023-10-06 17:12:56 +0300

    dev: task_1 done

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,25 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decode() {
+    const uniqeId = {uniqe: []};
+    const res = encoded.map(item => {
+        for (let key in item) {
+            const value = item[key];
+            if (key !== 'groupId' && key.includes('Id') && item[key] !== null) {
+                if (translations.hasOwnProperty(value)) {
+                    item[key] = translations[value];
+                } else {
+                    uniqeId.uniqe.push(value);
+                }
+            }
+        }
+        return item;
+    });
+    res.push(uniqeId);
+    return res;
+}
 
+const decoded = decode();
 
-// console.log(decoded)
+console.log(decoded)
